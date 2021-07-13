46e28668d945fb1ab0aa9659ebe9236a593ce28d
Vivi Cowan <vivicare@gmail.com>
2021-07-12 22:11:55 -0700

    used "require" for all necessary files

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const generateHTML = require("./src/generateHTML");
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const finalTeam = [];
+
+
